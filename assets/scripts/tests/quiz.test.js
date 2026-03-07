40d3d58150a7663ab1aea05b9e88e0eada868a36
CtrlAltKismet <Brookesdet@gmail.com>
2026-03-07 18:40:55 +0000

    Tested to ensure all quiz logic passes with jest, including next question.

--- assets/scripts/tests/quiz.test.js
+++ assets/scripts/tests/quiz.test.js
@@ -17,7 +17,23 @@ describe ("Quiz Logic Tests", () => {
 
         // Test to check what happens when an answer is wrong
         test("checkAnswer returns false when answer is incorrect", () => {
+
             //"B" is not the correct answer so function should return false
             expect(checkAnswer("B", "A")).toBe(false);
+        });
+
+        // Test to check updating score when answer is true
+        test("updateScore increases score when answer is correct", () => {
+            expect(updateScore(0, true)).toBe(1);
+        });
+
+        // Test to check score does not increase when answer is false
+        test("updateScore does not increase score when answer is wrong", () => {
+            expect(updateScore(2, false)).toBe(2);
+        });
+
+        // Test to check next question index increases
+        test("nextQuestion moves to next inded", () => {
+            expect(nextQuestion(5)).toBe(6);
         });
     });
