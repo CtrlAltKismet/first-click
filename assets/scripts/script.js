25476a01739918c2ab0d2dcee5741841b272c382
CtrlAltKismet <Brookesdet@gmail.com>
2026-03-21 19:33:11 +0000

    Added functionality and correctly styled hamburger icon for mobile devices.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,16 @@
+    // Mobile Nav Button Functionality
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuBtn = document.querySelector(".mobile-menu-btn");
+        const mainNav = document.querySelector(".main-nav");
+
+        if (menuBtn && mainNav) {
+            menuBtn.addEventListener("click", function () {
+                mainNav.classList.toggle("active");
+            });
+        }
+    });
+    
+    
     // Wikipedia API
 
     // Help page search button
